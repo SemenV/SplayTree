57394a6f9721c36495e87dabf8b4621409f4604d
qwe <asd>
2018-10-16 10:37:01 +0300

    fix splay function

--- src/SplayTree.java
+++ src/SplayTree.java
@@ -1,25 +1,24 @@
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.SortedSet;
+import java.util.*;
 
 public class SplayTree<T extends Comparable<T>> implements SortedSet<T> {
 
-    private Node root = null;
+    public Node root = null;
 
     public int count = 0;
 
     private class Node<E> {
         private final E value;
-        private Node<E> parent, left, right;
+        private Node<E> parent = null, left = null, right = null;
 
         Node(E value) {
             this.value = value;
         }
+
     }
+
 
     @Override
     public boolean add(T value) {
@@ -44,29 +43,22 @@ public class SplayTree<T extends Comparable<T>> implements SortedSet<T> {
         else if (newNode.value.compareTo(parentForCurrNode.value) < 0)
             parentForCurrNode.left = newNode;
         else parentForCurrNode.right = newNode;
-        splay(newNode);
+
+        splayy(newNode);
         count++;
         return true;
     }
 
-
-    @Nullable
-    private void splay(Node<T> node) {
-        if (node.parent == null) return;
-        while (node != root) {
+    private void splayy(Node<T> node) {
+        while (node.parent != null) {
+            if (node.parent == null) return;
 
             Node<T> parent = node.parent;
             Node<T> gparent = parent.parent;
 
             if (gparent == null) {
-                if (parent.left == node) {
-                    zig(parent, node);
-                    return;
-                } else {
-                    zig(parent, node);
-                    return;
-                }
-
+                zig(parent, node);
+                return;
             } else {
                 if (gparent.left == parent && parent.left == node ||
                         gparent.right == parent && parent.right == node) {
@@ -83,42 +75,46 @@ public class SplayTree<T extends Comparable<T>> implements SortedSet<T> {
     private void changeParent(Node<T> parent, Node<T> node) {
         Node<T> right = node.right;
         Node<T> left = node.left;
+        Node<T> parentParent = parent.parent;
 
         if (parent.left == node) {
             node.right = parent;
-            parent.left = node.right;
+            parent.left = right;
             if (right != null)
                 right.parent = parent;
-
         } else {
             node.left = parent;
-            parent.right = node.left;
+            parent.right = left;
             if (left != null)
                 left.parent = parent;
         }
+        node.parent = parentParent;
         parent.parent = node;
+
+        if (node.parent == null) root = node;
+        else {
+            if (parentParent.left != null && parentParent.left == parent)
+                parentParent.left = node;
+            if (parentParent.right != null && parentParent.right == parent)
+                parentParent.right = node;
+        }
     }
 
     private void zig(Node<T> parent, Node<T> node) {
         changeParent(parent, node);
-        node.parent = null;
+        root = node;
     }
 
     private void zigZig(Node<T> gparent, Node<T> parent, Node<T> node) {
-        Node<T> startParent = gparent.parent;
         changeParent(gparent, parent);
-        parent.parent = startParent;
         changeParent(parent, node);
-        node.parent = startParent;
     }
 
     private void zigZag(Node<T> gparent, Node<T> parent, Node<T> node) {
-        Node<T> startParent = parent.parent;
         changeParent(parent, node);
-        node.parent = startParent;
         changeParent(gparent, node);
-        node.parent = startParent;
     }
+
 
     private Node<T> search(T value) {
         return null;
